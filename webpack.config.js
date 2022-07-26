3d2613f15fbf12983419562a756732c9921f3917
Tushar <singhtushar348@gmail.com>
2022-07-26 12:41:27 +0100

    Add header with navlinks and logo

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,14 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      }
     ],
   },
 };
