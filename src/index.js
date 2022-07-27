8fb63bf3364231354472de67e754f689b42f8791
Tushar <singhtushar348@gmail.com>
2022-07-27 16:09:46 +0100

    Add render popup function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './style.css';
 import showApiUrl from './config/showsAPI';
 
+const modalSection = document.querySelector('.modal');
+const body = document.querySelector('body');
+
 // get shows from API source function
 const getShows = async () => {
   const res = await fetch(showApiUrl);
@@ -8,12 +11,14 @@ const getShows = async () => {
   return result;
 };
 
+//create show List
+
 const renderShows = async () => {
   const shows = await getShows(showApiUrl);
   const showSection = document.querySelector('.shows-container');
   shows.length = 20;
   document.querySelector('.loaderContainer').remove();
-  shows.forEach((show) => {
+  shows.forEach((show, index) => {
     const symbolContainer = document.createElement('span');
     const likesContainer = document.createElement('span');
     const showContainer = document.createElement('div');
@@ -36,6 +41,11 @@ const renderShows = async () => {
     symbolContainer.innerHTML = '&#9825;';
     likesContainer.innerText = '100';
     commentBtn.textContent = 'comments';
+    commentBtn.addEventListener('click', ()=>{
+      modalSection.style.display = 'block';
+      body.style.overflowY = 'hidden'
+      renderPopup(index);
+    })
 
     figcaption.appendChild(p);
     figcaption.appendChild(symbolContainer);
@@ -51,3 +61,40 @@ const renderShows = async () => {
 };
 
 renderShows();
+
+//create popup
+
+async function renderPopup(i){
+  const show = await getShows();
+  modalSection.innerHTML = '';
+    const popupContainer = document.createElement('div');
+    const closeBtn = document.createElement('button');
+    const popupFig = document.createElement('figure');
+    const popupImage = document.createElement('img');
+    const figcaption = document.createElement('figcaption');
+    const popupShowTitle = document.createElement('h3');
+    
+    popupContainer.classList.add('popup-container');
+    closeBtn.classList.add('close-popup');
+    figcaption.classList.add('popup-details');
+    popupImage.classList.add('popup-img');
+
+    popupImage.setAttribute('src', show[i].image.original);
+    popupImage.setAttribute('alt', `${show[i].name} image`);
+    
+    closeBtn.innerHTML = '&times;';
+    popupShowTitle.innerHTML = show[i].name;
+    figcaption.innerHTML = show[i].summary;
+
+    figcaption.appendChild(popupShowTitle);
+    popupFig.appendChild(popupImage);
+    popupFig.appendChild(figcaption);
+    popupContainer.appendChild(closeBtn);
+    popupContainer.appendChild(popupFig);
+
+    modalSection.appendChild(popupContainer);
+    closeBtn.addEventListener('click', ()=>{
+      modalSection.style.display = 'none';
+      body.style.overflowY = 'auto'
+    })
+};
